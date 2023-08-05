75fb6c93147d43ba06d824066eaece5b736114f6
CarlosIDJM <carlosidjmdz@gmail.com>
2023-08-05 00:28:27 -0600

    carga de programa de Hola Mundo y Mi nombre

---
+++ HolaMundo_01.java
@@ -0,0 +1,5 @@
+public class HolaMundo_01 {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ MiNombre_02.java
@@ -0,0 +1,11 @@
+public class MiNombre_02{
+    public static void main (String args[]) {
+
+        String Nombre="Carlos Ivan", Ap="De Jesus", Am="Mendoza";
+        Integer Edad=29, Mes=10, Dia=1, Ano=2023;
+        
+        System.out.print("Mi edad es:" + Edad+ "\n");
+        System.out.println("Mi Nombre es: " + Nombre +" "+ Ap + " " + Am );
+
+    }
+}
