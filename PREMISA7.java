2905db33ac671d116dc1de133bcc510b58dc54ac
CarlosIDJM <carlosidjmdz@gmail.com>
2023-08-12 21:43:07 -0600

    PREMISA 7 COSTO DE BOLETO DE CINE

--- PREMISA7.java
+++ PREMISA7.java
@@ -1,16 +1,25 @@
 import java.util.Scanner;
 
 public class PREMISA7 {
+    /*Calcular el costo de un boleto de cine segun la edad del espectador
+     * aplicando diferentes tarifas para niños, adultos, y personas mayores
+     */
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int edad;
 
-        System.out.println("Ingresa tu edad");
+        System.out.println("Introduce la edad el espectador: ");
         edad = sc.nextInt();
 
-        if(edad >= 0 && edad <= 15) {
-            System.out.println("El costo de l boleto es 35 peros");
-        } else if (edad )
-
+        if(edad >= 3 && edad <= 14) {
+            System.out.println("El costo de l boleto es 35 pesos");
+        } else if (edad >=15 && edad <=60) {
+            System.out.println("El costo de l boleto es 80 pesos");
+        } else if(edad >= 61){
+            System.out.println("El costo de l boleto es 50 pesos");
+        } else {
+            System.out.println("Introduce un valor valido");
+        }
+        sc.close();
     }
 }
