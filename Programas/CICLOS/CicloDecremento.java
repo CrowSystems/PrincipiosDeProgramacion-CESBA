ef2f746c298f760608a5ae65bd599ee950d90726
CarlosIDJM <carlosidjmdz@gmail.com>
2023-09-02 14:37:04 -0600

    Ejemplo de ciclos for

---
+++ Programas/CICLOS/CicloDecremento.java
@@ -0,0 +1,10 @@
+package CICLOS;
+
+public class CicloDecremento {
+    public static void main(String[] args) {
+        int i;
+        for(i=10; i>=0; i--){
+            System.out.println(i);
+        }
+    }   
+}
---
+++ Programas/CICLOS/CicloIncremento.java
@@ -0,0 +1,12 @@
+package CICLOS;
+
+public class CicloIncremento {
+
+    public static void main(String[] args) {
+        
+        int i;
+        for(i=0;i<=10;i++){
+            System.out.println(i);
+        }
+    }    
+}
