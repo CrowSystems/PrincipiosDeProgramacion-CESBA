495f8c771328c0c44477ac5a53b41db8fc35fdf4
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-09 23:35:30 -0600

    carga de matrices

---
+++ $5Arreglos_Matrices/Clases/Arreglo3x3Letras.java
@@ -0,0 +1,19 @@
+package $5Arreglos_Matrices.Clases;
+
+import java.util.Scanner;
+
+public class Arreglo3x3Letras {
+    
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        //Declaración de variable simple
+        int numero[][] = new int [2][2];
+        int i, j;
+
+
+        System.out.println("Ingresa los elementos de arreglo dibimencional 2 x 2: ");
+        
+    }
+}
