3a3087dba078b7b438876b05637c27137af10c4b
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-10 07:16:35 -0600

    carga de arreglo de 3 x 3

--- $5Arreglos_Matrices/Clases/Arreglo3x3Letras.java
+++ $5Arreglos_Matrices/Clases/Arreglo3x3Letras.java
@@ -3,17 +3,29 @@ package $5Arreglos_Matrices.Clases;
 import java.util.Scanner;
 
 public class Arreglo3x3Letras {
-    
     public static void main(String[] args) {
-
+        //Declaracion de variable Scanner
         Scanner sc = new Scanner(System.in);
 
-        //Declaración de variable simple
-        int numero[][] = new int [2][2];
+        //Declaración de variables simple y arreglos
+        int numeros[][] = new int [3][3];
         int i, j;
-
 
         System.out.println("Ingresa los elementos de arreglo dibimencional 2 x 2: ");
         
+        for(i=0; i < 2; i++) {
+            for(j=0; j<2; j++){
+                System.out.println("Ingresa el valor");
+                numeros[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("El arreglo bidimencional ingresado: ");
+        for(i=0; i<2;i++){
+            System.out.println(" ");
+            for(j=0;j<2;j++){
+                System.out.println("El Arreglo en la posición: "+"["+i+","+j+"] es: "+numeros[i][j]);
+            }
+        }
+        sc.close();
     }
 }
