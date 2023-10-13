41c90445b1e5a561ae41e3a33c4d172da34203dd
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-12 21:40:49 -0600

    Programa 3, conteo de un numero fijo

--- $5Arreglos_Matrices/$2Ejercicios/P4ContadorNumeros.java
+++ $5Arreglos_Matrices/$2Ejercicios/P4ContadorNumeros.java
@@ -8,9 +8,28 @@ package $5Arreglos_Matrices.$2Ejercicios;
  * específico en un arreglo unidimensional de cadenas.
  */
 
+import java.util.Scanner;
+
 public class P4ContadorNumeros {
-public static void main(String[] args) {
+    public static void main(String[] args) {
+        //Declaración de variables Scanner
+        Scanner sc = new Scanner(System.in);
     
-}
-    
+        //Variables simples
+        int i, j=0, k, cont=0;
+
+        System.out.println(("Cuantos valores deseas ingresar."));
+        j = sc.nextInt();
+
+        //Declaración de un arreglo
+        int numeros[] = new int[j];
+
+        for(i=0; i< j; i++){
+            System.out.print("Ingresa valores para el arreglo: ");
+            numeros[i] = sc.nextInt();
+        }
+
+        System.out.print("Que numero deseas buscar: ");
+        k = sc.nextInt();
+    }
 }
