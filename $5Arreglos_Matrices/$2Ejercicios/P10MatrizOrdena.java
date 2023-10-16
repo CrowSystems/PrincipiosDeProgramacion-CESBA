562ec7765984f7ce15760467d0166a5fd0f7d0e7
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-16 07:26:42 -0600

    Carga de Ejercicio 10 Matriz de ordenamiento

--- $5Arreglos_Matrices/$2Ejercicios/P10MatrizOrdena.java
+++ $5Arreglos_Matrices/$2Ejercicios/P10MatrizOrdena.java
@@ -1,5 +1,50 @@
+/*
+ * Author: Carlos Ivan de Jesus Mendoza
+ * Grupo: 2A
+ * Principios de programación
+ * 
+ * Implementa un algoritmo de ordenación 
+ * (por ejemplo, ordenación de burbuja o ordenación rápida) 
+ * para ordenar una matriz bidimensional en función de una columna específica. 
+ * El usuario debe elegir la columna por la que se ordenará.
+ * 
+*/
 package $5Arreglos_Matrices.$2Ejercicios;
 
+import java.util.Scanner;
+
 public class P10MatrizOrdena {
-    
+    public static void main(String[] args) {
+        //Declaración de variable de solicitud de datos.
+        Scanner sc=new Scanner(System.in);
+
+        //Declaración de variables simples
+        int i,j, N=0, columna=0;
+
+
+        System.out.println("Ingresa el tamaño de tu arreglo: ");
+        N = sc.nextInt();
+        //Delcaración de arreglo
+        int arreglo1[][] = new int[N][N];
+
+        //Arrgelo 1
+        System.out.println("Ingresa los valores para la matriz 1: ");
+        for(i=0; i<N;i++){
+            System.out.print("");
+            for(j=0; j<N; j++){
+                System.out.print("Valor:  ");
+                arreglo1[i][j] = sc.nextInt(); 
+            }
+        }
+        //Impresión de arreglo
+        for(i=0; i<N;i++){
+            System.out.println("");
+            for(j=0; j<N; j++){
+                System.out.print("["+arreglo1[i][j]+"]");
+            }
+        }
+
+        //Ordena el arreglo
+        System.out.println("Ingre el numero de la columna que desea ordenar: ");
+    }
 }
