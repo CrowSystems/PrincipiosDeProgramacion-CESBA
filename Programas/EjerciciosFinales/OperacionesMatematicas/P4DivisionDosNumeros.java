01711f5583575c82e6f3cf9e6515b0b4dead012e
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-21 08:30:14 -0600

    División de dos numeros entreros

--- Programas/EjerciciosFinales/OperacionesMatematicas/P4DivisionDosNumeros.java
+++ Programas/EjerciciosFinales/OperacionesMatematicas/P4DivisionDosNumeros.java
@@ -1,5 +1,23 @@
 package EjerciciosFinales.OperacionesMatematicas;
 
+import java.util.Scanner;
+
 public class P4DivisionDosNumeros {
+    public static void main(String[] args) {
+
+        Scanner sc =  new Scanner(System.in);
+        int num1, num2, total;
+
+        System.out.println("Ingresa el numero 1: ");
+        num1 = sc.nextInt();
+
+        System.out.println("Ingresa el numero 2: ");
+        num2 = sc.nextInt();
+
+        total = num1 / num2;
+        System.out.println("Resultado "+total);
+
+        sc.close();
+    }
     
 }
--- Programas/EjerciciosFinales/OperacionesMatematicas/P8AreaCirculo.java
+++ Programas/EjerciciosFinales/OperacionesMatematicas/P8AreaCirculo.java
@@ -1,5 +1,5 @@
 package EjerciciosFinales.OperacionesMatematicas;
 
-public class P7PotenciaDeUnNumero {
+public class P8AreaCirculo {
     
 }
