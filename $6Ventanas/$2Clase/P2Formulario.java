ed525a3f06d86600a20faca421891696c642c25a
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-10-28 14:08:49 -0600

    manejo de frames

---
+++ $6Ventanas/$2Clase/P2Formulario.java
@@ -0,0 +1,17 @@
+package $6Ventanas.$2Clase;
+
+import javax.swing.JFrame;
+
+public class P2Formulario extends JFrame {
+    public P2Formulario(){
+        setLayout(null);
+    }
+
+    public static void main(String[] args) {
+        P2Formulario formulario1 = new P2Formulario();
+        formulario1.setBounds(0,0,400,550);
+        formulario1.setVisible(true);
+        formulario1.setLocationRelativeTo(null);
+        formulario1.setResizable(false);
+    }
+}
