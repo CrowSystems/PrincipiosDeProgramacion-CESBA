e3c90350b39d410e8d988c008e08c4a5ce3bf58f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-03 17:55:04 -0600

    Area de un circulo

--- $10_EjerciciosFinales/OperacionesMatematicas/P8AreaCirculo.java
+++ $10_EjerciciosFinales/OperacionesMatematicas/P8AreaCirculo.java
@@ -1,5 +1,20 @@
 package $10_EjerciciosFinales.OperacionesMatematicas;
 
+import java.util.Scanner;
+
 public class P8AreaCirculo {
-    
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        double num1, area;
+
+        System.out.println("*************** Area de un circulo ****************");
+
+        System.out.println("Ingresa el tamaño del radio: ");
+        num1 = sc.nextDouble();
+
+        area = Math.PI * Math.pow(num1,2);
+
+        System.out.println("El are del circulo es: " +  area);
+        sc.close();
+    }  
 }
