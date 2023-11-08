06c089474ff27cbadfd827268a442e429f4bf481
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-07 23:51:03 -0600

    perimetro de un triangulo

--- $10_EjerciciosFinales/NIVELX/P9PerimetrodeunTriangulo.java
+++ $10_EjerciciosFinales/NIVELX/P9PerimetrodeunTriangulo.java
@@ -1,1 +1,22 @@
 package $10_EjerciciosFinales.NIVELX;
+
+import java.util.Scanner;
+
+public class P9PerimetrodeunTriangulo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1, perimetro;
+
+        System.out.println("**************PERIMETRO DE UN TRIANGULO*******************");
+
+        System.out.println("Ingresa uno de los lados del triangulo: ");
+        num1 = sc.nextDouble();
+
+        perimetro = 3*num1;
+
+        System.out.println("El perimetro de tu triangulo es de: "+perimetro);
+
+        sc.close();
+    }
+}
