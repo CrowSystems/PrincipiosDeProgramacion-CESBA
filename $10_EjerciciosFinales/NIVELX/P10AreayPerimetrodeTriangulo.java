9756900faeb80daefd9ee850096acd6598fe4476
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-08 23:47:59 -0600

    perimetro de un triangulo

--- $10_EjerciciosFinales/NIVELX/P10AreayPerimetrodeTriangulo.java
+++ $10_EjerciciosFinales/NIVELX/P10AreayPerimetrodeTriangulo.java
@@ -1,5 +1,14 @@
 package $10_EjerciciosFinales.NIVELX;
 
+import java.util.Scanner;
+
+/*
+ * Calcular el área y perímetro de un triángulo
+ */
 public class P10AreayPerimetrodeTriangulo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
     
 }
