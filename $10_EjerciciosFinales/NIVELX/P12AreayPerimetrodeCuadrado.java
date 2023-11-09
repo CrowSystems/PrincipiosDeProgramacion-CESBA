4ba1d544098b1b697bf75adc070172e2eb16157f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-09 00:24:04 -0600

    area y perimetro de un rectangulo

--- $10_EjerciciosFinales/NIVELX/P12AreayPerimetrodeCuadrado.java
+++ $10_EjerciciosFinales/NIVELX/P12AreayPerimetrodeCuadrado.java
@@ -1,5 +1,27 @@
 package $10_EjerciciosFinales.NIVELX;
 
+import java.util.Scanner;
+
+/*
+ * Calcular el área y el perímetro de un rectángulo.
+ */
 public class P12AreayPerimetrodeCuadrado {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double base, altura, area, perimetro;
+
+        System.out.println("************ Area y perimetro de un rectangulo *************");
+
+        System.out.println("Ingresa la base del rectangulo: ");
+        base = sc.nextDouble();
+
+        System.out.println("Ingresa la altura del rectangulo: ");
+        altura = sc.nextDouble();
+
+        area =  base * altura;
+        perimetro = (2 * base) + (altura * 2);
+
+        System.out.println("El area del rectangulo es: "+area);
+        System.out.println("El perimetro del rectangulo es: "+perimetro);
+    }
 }
