22ba4263013c0563c71fcb9807e9045d38821db8
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-08 23:59:23 -0600

    area y perimetro de un triangulo

--- $10_EjerciciosFinales/NIVELX/P10AreayPerimetrodeTriangulo.java
+++ $10_EjerciciosFinales/NIVELX/P10AreayPerimetrodeTriangulo.java
@@ -8,7 +8,21 @@ import java.util.Scanner;
 public class P10AreayPerimetrodeTriangulo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        double base, altura, area, perimetro;
+
+        System.out.println("************** Area y Perimetro de un triangulo*************");
+
+        System.out.print("Ingresa la base del triangulo: ");
+        base = sc.nextDouble();
+
+        System.out.print("Ingresa la altura del triangulo: ");
+        altura = sc.nextDouble();
+
+        area = (base * altura ) / 2;
+        perimetro = base * 3;
+
+        System.out.print("El area del triangulo es: "+area);
+        System.out.print("El perimetro del triangulo es: "+perimetro);
     }
     
 }
