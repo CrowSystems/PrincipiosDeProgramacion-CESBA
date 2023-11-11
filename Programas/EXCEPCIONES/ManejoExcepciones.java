0506dafaa2ae251995039dc8bb9071ebd41d4995
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-11 15:01:51 -0600

    Manejo de excepciones

---
+++ Programas/EXCEPCIONES/ManejoExcepciones.java
@@ -0,0 +1,23 @@
+package EXCEPCIONES;
+
+public class ManejoExcepciones {
+    public static void main(String[] args) {
+        try {
+            //Excepción
+            int resultado = dividir(10,0);
+        } catch (ArithmeticException e) {
+            //Captura la excepcion aritmetica.
+            System.out.println("Error al dividir entre cero.");
+        } finally {
+            //Este bloque para mensaje de finalización
+            System.out.println("Operacion Completada");
+
+        }
+    }
+
+    public static int dividir(int numerador, int denominador) {
+        //Metodo que lanza la excepción
+        return numerador / denominador;
+    }
+    
+}
