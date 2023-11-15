d1d1dde4d2850183d2e82047380d17a24e69ca68
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-14 22:47:18 -0600

    area y volumen de un prisma rectangular

--- $10_EjerciciosFinales/NIVELX/P16AreayVolumenPrismaRectangular.java
+++ $10_EjerciciosFinales/NIVELX/P16AreayVolumenPrismaRectangular.java
@@ -1,7 +1,25 @@
 package $10_EjerciciosFinales.NIVELX;
 
+import java.util.Scanner;
+
 public class P16AreayVolumenPrismaRectangular {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a, b, c, area, volumen;
+
+        System.out.println("Introduce el valor de la altura: ");
+        a = sc.nextDouble();
+
+        System.out.println("Introduce el valor de la base: ");
+        b = sc.nextDouble();
+
+        System.out.println("Introduce el valor de lo ancho: ");
+        c = sc.nextDouble();
+
+        volumen = (b * c) * a;
+        area = 2*((b * c)+(b * a)+(a * c));
         
+        System.out.println("El valor del área es: "+area);
+        System.out.println("El valor del volumen es: "+volumen);       
     }    
 }
