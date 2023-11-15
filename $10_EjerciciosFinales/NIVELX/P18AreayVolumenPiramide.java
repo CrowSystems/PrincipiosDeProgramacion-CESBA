9075f8810050c075b861886fc5cb0f03d708f524
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-14 23:26:16 -0600

    Area y volumen de una piramide

--- $10_EjerciciosFinales/NIVELX/P18AreayVolumenPiramide.java
+++ $10_EjerciciosFinales/NIVELX/P18AreayVolumenPiramide.java
@@ -1,7 +1,26 @@
 package $10_EjerciciosFinales.NIVELX;
 
+import java.util.Scanner;
+
 public class P18AreayVolumenPiramide {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        double area, volumen, altitud, base;
+
+        System.out.println("******** Area y Volumen Piramide *********");
+
+        System.out.println("Introduce el tamaño de la base: ");
+        base = sc.nextDouble();
+
+        System.out.println("Introduce la altitud de la piramide: ");
+        altitud = sc.nextDouble();
+
+        area = Math.pow(base, 2) + (2*(base))*altitud;
+        volumen = (1/3*(Math.pow(base, 2))) * altitud;
+
+        System.out.println("El area area de la piramide es: " +area);
+        System.out.println("El volumen de la piramide es: " + volumen);
+
+        sc.close();
     }
 }
