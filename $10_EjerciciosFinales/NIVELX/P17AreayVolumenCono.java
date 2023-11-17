042e5e83a67edf0fe6f09560d0cc086d315c5a2f
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-16 23:31:00 -0600

    area y volumen de un cono

--- $10_EjerciciosFinales/NIVELX/P17AreayVolumenCono.java
+++ $10_EjerciciosFinales/NIVELX/P17AreayVolumenCono.java
@@ -1,7 +1,19 @@
 package $10_EjerciciosFinales.NIVELX;
 
+import java.util.Scanner;
+
 public class P17AreayVolumenCono {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r, h, g, area, volumen;
+
+        System.out.println("********Area y Volumen de un cono ***********");
+
+        System.out.println("Introduce el valor del radio: ");
+        r = sc.nextDouble();
+
+        
+
         
     }    
 }
