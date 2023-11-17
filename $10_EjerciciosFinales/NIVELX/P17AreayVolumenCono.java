936961627296eae83adc493427dc37628646e0c2
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-16 23:42:29 -0600

    area y volumen de un cono 2

--- $10_EjerciciosFinales/NIVELX/P17AreayVolumenCono.java
+++ $10_EjerciciosFinales/NIVELX/P17AreayVolumenCono.java
@@ -12,7 +12,17 @@ public class P17AreayVolumenCono {
         System.out.println("Introduce el valor del radio: ");
         r = sc.nextDouble();
 
-        
+        System.out.println("Introduce el valor de la altura: ");
+        h = sc.nextDouble();
+
+        System.out.println("Introduce el valor de generatriz: ");
+        g = sc.nextDouble();
+
+        area = Math.PI * (r*(r + g));
+        volumen = ((1/3)*(Math.PI)) * Math.pow(r, 2) * h;
+
+        System.out.println("El valor del area es: "+area);
+        System.out.println("El valor del volumen: "+volumen);
 
         
     }    
