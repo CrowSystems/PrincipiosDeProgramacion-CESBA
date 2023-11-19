d6a1e2f9c81a6483fe81d77a2df8dc6ee48fbfa7
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-18 21:11:33 -0600

    programa 20 Calular el MCD

--- $10_EjerciciosFinales/NIVELY/P20CalcularMCD.java
+++ $10_EjerciciosFinales/NIVELY/P20CalcularMCD.java
@@ -1,10 +1,30 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Calcular el máximo común divisor (MCD) de dos números enteros.
  */
 public class P20CalcularMCD {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, valor;
+
+        System.out.println("************ Maximo como un divisor de dos números *********");
+
+        System.out.println("Introduce el primer número: ");
+        a = sc.nextInt();
+
+        System.out.println("Introduce el segundo número: ");
+        b = sc.nextInt();
+
+        while (b != 0) {
+            valor = b;
+            b = a % b;
+            a = valor;
+        }
         
+        System.out.print("El maximo como un divisor es: "+a);
+        sc.close();
     }
-    
 }
