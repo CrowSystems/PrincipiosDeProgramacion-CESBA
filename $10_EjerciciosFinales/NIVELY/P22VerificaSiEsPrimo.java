3ed4cd302a7eb26f4b9fe1f551c114536178670e
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-19 16:21:34 -0600

    Programa 22 Numero primo

--- $10_EjerciciosFinales/NIVELY/P22VerificaSiEsPrimo.java
+++ $10_EjerciciosFinales/NIVELY/P22VerificaSiEsPrimo.java
@@ -1,9 +1,26 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Verificar si un número es primo.
  */
 public class P22VerificaSiEsPrimo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, i, valor;
+
+        System.out.println("******** Numero primo ********");
         
-    }
+        System.out.println("Introduce el numero a evaluar mayor a 0 y entero: ");
+        a = sc.nextInt();
+
+        if(a == 1){
+            System.out.println("No es primo");
+        } else if(a % 2 != 0) {
+            System.out.println("El numero " + a + " es primo");
+        } else {
+            System.out.println("No es primo");
+        }
+    } 
 }
