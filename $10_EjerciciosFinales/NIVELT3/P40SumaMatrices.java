c8b19a87511d34badd27868555ed26b6756a2ccc
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-20 23:03:02 -0600

    carga de programas inicial

---
+++ $10_EjerciciosFinales/NIVELT3/P40SumaMatrices.java
@@ -0,0 +1,10 @@
+package $10_EjerciciosFinales.NIVELT3;
+/*
+ * Calcular la suma de dos matrices.
+ */
+public class P40SumaMatrices {
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ $10_EjerciciosFinales/NIVELT3/P41ProductoMatrices.java
@@ -0,0 +1,10 @@
+package $10_EjerciciosFinales.NIVELT3;
+/*
+ * Calcular el producto de dos matrices.
+ */
+public class P41ProductoMatrices {
+    public static void main(String[] args) {
+        
+    }
+    
+}
