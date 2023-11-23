1452e35145f7d8a1d73922c3b227d688c0bacbfe
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-22 23:40:57 -0600

    Programa 25 convercion de Celcius a Fahrenheit

--- $10_EjerciciosFinales/NIVELY/P25GradosCaF.java
+++ $10_EjerciciosFinales/NIVELY/P25GradosCaF.java
@@ -1,9 +1,25 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Convertir grados Celsius a grados Fahrenheit.
  */
 public class P25GradosCaF {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double g, f;
+
+        System.out.println("************** Conversión de grados Celsius a grados Fahrenheit *************");
         
+        System.out.println("Introduce los grados a convertir");
+        g = sc.nextDouble();
+
+        f = (g*(1.8))+32; 
+
+        System.out.println("Celcius: "+g);
+        System.out.println("Fahrenheit: "+f);
+
+        sc.close();
     }
 }
