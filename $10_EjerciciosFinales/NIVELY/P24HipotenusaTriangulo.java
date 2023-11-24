a754ef32993ea3890b0ee7b826b7cee5f5be9d5a
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-23 23:20:46 -0600

    Programa 24 Calculo de la Hipotenusa del triangulo

--- $10_EjerciciosFinales/NIVELY/P24HipotenusaTriangulo.java
+++ $10_EjerciciosFinales/NIVELY/P24HipotenusaTriangulo.java
@@ -1,9 +1,27 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Calcular la hipotenusa de un triángulo rectángulo.
  */
 public class P24HipotenusaTriangulo {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double Hipo, a, b;
+
+        System.out.println("***** Hipotenusa de un triangulo *****");
+
+        System.out.println("Introduce el cateto opuesto: ");
+        a = sc.nextDouble();
         
+        System.out.println("Introduce el cateto adyacente: ");
+        b = sc.nextDouble();
+
+        Hipo = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+        System.out.println("El valor de la hipotenusa es: "+Hipo);
+
+        sc.close();
     }
 }
