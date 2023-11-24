7fdeac8cea78c977046f0a1c0e8af8e4d7c5a544
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-23 23:51:48 -0600

    Programa 28 Factorial de un numero

--- $10_EjerciciosFinales/NIVELY/P28FactorialNumero.java
+++ $10_EjerciciosFinales/NIVELY/P28FactorialNumero.java
@@ -1,7 +1,28 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Calcular el factorial de un número entero.
  */
 public class P28FactorialNumero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, i, factorial=1;
+
+        System.out.println("***** Factorial de un número entero");
+
+        System.out.println("Introduce el numero a calcular: ");
+        a = sc.nextInt();
+
+        for(i = 1; i <= a; i++) {
+            factorial = factorial * i;
+        }
+
+        System.out.println("El factorial de "+a+" es: "+factorial);
+
+        sc.close();
+
+    }
     
 }
