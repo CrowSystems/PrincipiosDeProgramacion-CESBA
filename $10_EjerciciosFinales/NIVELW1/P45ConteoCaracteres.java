009658e4d206c8a10e699d884f20dd69bba63e4a
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-25 17:44:11 -0600

    Programa 45 Concatenar Caracteres

--- $10_EjerciciosFinales/NIVELW1/P45ConteoCaracteres.java
+++ $10_EjerciciosFinales/NIVELW1/P45ConteoCaracteres.java
@@ -1,7 +1,26 @@
 package $10_EjerciciosFinales.NIVELW1;
+
+import java.util.Scanner;
+
 /*
  * Contar la cantidad de caracteres en una cadena.
  */
 public class P45ConteoCaracteres {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a, b;
+
+        System.out.println("***** Concaten dos lineas de caracteres *****");
+
+        System.out.println("Introduce el primer caracter: ");
+        a = sc.nextLine();
+
+        System.out.println("Introduce el segundo caracter: ");
+        b = sc.nextLine();
+
+        System.out.println("Las lineas de caracteres quedan de la siguiente manera: ");
+        System.out.println(a + " " +b);
+
+        sc.nextLine();
+    }
 }
