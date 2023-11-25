549dcb0ff01598d78421878ce6ccbccb476697eb
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-25 17:53:00 -0600

    Programa 46 Contar cantidad caracteres en una palabra

--- $10_EjerciciosFinales/NIVELW1/P46ConteoPalabras.java
+++ $10_EjerciciosFinales/NIVELW1/P46ConteoPalabras.java
@@ -1,10 +1,22 @@
 package $10_EjerciciosFinales.NIVELW1;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
 /*
  * Contar la cantidad de palabras en una cadena.
  */
 public class P46ConteoPalabras {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a;
+
+        System.out.println("Introduce la frace para contar: ");
+        a = sc.nextLine();
+
+        StringTokenizer st = new StringTokenizer(a);
+        System.out.println("El número de parabaras son: "+st.countTokens());
         
-        
+        sc.close();
     }
 }
