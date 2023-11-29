9caa9eefa40096f736156bfc4358b055b92ccf56
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-11-29 17:10:03 -0600

    Programa 29 Identificar añio bisiesto.

--- $10_EjerciciosFinales/NIVELY/P29VerificaAñoBisiesto.java
+++ $10_EjerciciosFinales/NIVELY/P29VerificaAñoBisiesto.java
@@ -1,7 +1,24 @@
 package $10_EjerciciosFinales.NIVELY;
+
+import java.util.Scanner;
+
 /*
  * Verificar si un año es bisiesto o no.
  */
 public class P29VerificaAñoBisiesto {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year;
+
+        System.out.println("***** Evaluación de año si es biciesto *****");
+        
+        System.out.println("Ingresar el año a evaluar: ");
+        year = sc.nextInt();
+
+        if((year %400 == 0) || ((year %4 == 0) && year %100 !=0)){
+            System.out.println("El año: "+year+" es Bisiesto");
+        } else {
+            System.out.println("El año: "+year+" No es bisiesto");
+        }
+    }
 }
