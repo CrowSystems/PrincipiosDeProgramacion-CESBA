d8cfc89cf7ad9ee2c2d252e75585496c2f252008
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-01 20:11:59 -0600

    Programa 59 N numeros naturales con metodo while

--- $10_EjerciciosFinales/CICLOS_C1/C1/P59NnumerosWhiles.java
+++ $10_EjerciciosFinales/CICLOS_C1/C1/P59NnumerosWhiles.java
@@ -1,10 +1,23 @@
 package $10_EjerciciosFinales.CICLOS_C1.C1;
+
+import java.util.Scanner;
+
 /*
  * Imprimir los N primeros números naturales usando un bucle while.
  */
 public class P59NnumerosWhiles {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i=1;
         
+        System.out.println("***** N numeros naturales *****");
+        System.out.println("Ingresa hasta que numero deseas visualizar: ");
+        n = sc.nextInt();
+
+        while (i<n) {
+            System.out.println(i+" es natural");
+            i++;
+        }
+        sc.close();
     }
-    
 }
