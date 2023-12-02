19843a50b9c64a7f43c21418bb2a5ef81b271bf8
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-01 19:39:03 -0600

    Programa 63 Tablas de multiplicar

--- $10_EjerciciosFinales/CICLOS_C1/C1/P62NnumeroImpares.java
+++ $10_EjerciciosFinales/CICLOS_C1/C1/P62NnumeroImpares.java
@@ -2,7 +2,7 @@ package $10_EjerciciosFinales.CICLOS_C1.C1;
 /*
  * Imprimir los N primeros números impares.
  */
-public class P61NnumeroImpares {
+public class P62NnumeroImpares {
     public static void main(String[] args) {
     
     }
--- $10_EjerciciosFinales/CICLOS_C1/C1/P63TabladeMultiplicar.java
+++ $10_EjerciciosFinales/CICLOS_C1/C1/P63TabladeMultiplicar.java
@@ -1,9 +1,22 @@
 package $10_EjerciciosFinales.CICLOS_C1.C1;
+
+import java.util.Scanner;
 /*
  * Imprimir la tabla de multiplicar de un número dado.
  */
 public class P63TabladeMultiplicar {
    public static void main(String[] args) {
-    
-   } 
+    Scanner sc = new Scanner(System.in);
+    int n,i,res;
+
+      System.out.println("***** Tablas de multiplicar *****");
+
+      System.out.println("Ingresa la tabla a multiplicar: ");
+      n =sc.nextInt();
+      System.out.println("TABLA DEL NÚMERO "+n);
+      for(i=1; i <= 10; i++){
+         res = n * i;
+         System.out.println(n+" x "+i+" = "+res);
+      }
+   }
 }
