093a124c2b9bea23384a0bb21b8cf84fc6eda3ae
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-01 22:05:05 -0600

    Programa 71 y 72 año bisiesto y par o impar con while

--- $10_EjerciciosFinales/D1/P71AnoBisiesto.java
+++ $10_EjerciciosFinales/D1/P71AnoBisiesto.java
@@ -1,7 +1,23 @@
 package $10_EjerciciosFinales.D1;
+
+import java.util.Scanner;
+
 /*
  * Verificar si un año es bisiesto o no.
  */
 public class P71AnoBisiesto {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.println("***** Identifica año biciesto *****");
+        System.out.println("Ingresa el año a evaluar: ");
+        n = sc.nextInt();
+        
+        if ((n % 4 == 0) && ((n % 100 != 0) || (n % 400 == 0))){
+	        System.out.println("El año es bisiesto");
+        } else {
+            System.out.println("El año no es bisiesto");
+        }
+    }
 }
--- $10_EjerciciosFinales/D1/P72NumeroPrimo.java
+++ $10_EjerciciosFinales/D1/P72NumeroPrimo.java
@@ -1,7 +1,31 @@
 package $10_EjerciciosFinales.D1;
+
+import java.util.Scanner;
+
 /*
  * Verificar si un número es primo usando un bucle while.
  */
 public class P72NumeroPrimo {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+        do {
+            System.out.print("Introduce un numero >0: ");
+            n = sc.nextInt();
+        } while (n <= 0);
+		
+        if (n == 1) {
+            System.out.println("No es primo");
+        } else {
+            i = 2;   
+            while (n % i != 0) {                           
+                i++;
+            }
+            if (i == n) {
+                System.out.println("Primo");        
+            } else {
+                System.out.println("No es primo");  
+            }
+        }
+    }
 }
