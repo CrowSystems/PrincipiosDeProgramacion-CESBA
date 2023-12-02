24e877db6201925d1baea7721c3e2be49ebc8feb
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-01 19:59:03 -0600

    Programa 61 Numeros pares

--- $10_EjerciciosFinales/CICLOS_C1/C1/P61NnumerosPares.java
+++ $10_EjerciciosFinales/CICLOS_C1/C1/P61NnumerosPares.java
@@ -1,7 +1,26 @@
 package $10_EjerciciosFinales.CICLOS_C1.C1;
+
+import java.util.Scanner;
+
 /*
  * Imprimir los N primeros números pares.
  */
 public class P61NnumerosPares {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+
+        System.out.println("***** Numeros pares. *****");
+        System.out.println("Ingresa hasta que nuemero deseas visualizar: ");
+        n = sc.nextInt();
+        
+        System.out.println("Los numeros impares son: ");
+        for(i=1; i<=n; i++){
+            if(i%2==0){
+                System.out.println(i + " es par");
+            }
+        }
+        sc.close();
+    }
     
 }
