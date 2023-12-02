919644f231e2a099bd993137088a231d6468f0d6
CARLOSIJM <69187869+CrowSystems@users.noreply.github.com>
2023-12-01 19:56:50 -0600

    Programa 62 Numeros impares

--- $10_EjerciciosFinales/CICLOS_C1/C1/P62NnumeroImpares.java
+++ $10_EjerciciosFinales/CICLOS_C1/C1/P62NnumeroImpares.java
@@ -1,10 +1,25 @@
 package $10_EjerciciosFinales.CICLOS_C1.C1;
+
+import java.util.Scanner;
+
 /*
  * Imprimir los N primeros números impares.
  */
 public class P62NnumeroImpares {
     public static void main(String[] args) {
-    
+        Scanner sc = new Scanner(System.in);
+        int n, i;
+
+        System.out.println("***** Numeros impares. *****");
+        System.out.println("Ingresa hasta que nuemero deseas visualizar: ");
+        n = sc.nextInt();
+        
+        System.out.println("Los numeros impares son: ");
+        for(i=0; i<=n; i++){
+            if(i%2!=0){
+                System.out.println(i + " es impar");
+            }
+        }
+        sc.close();
     }
-    
 }
